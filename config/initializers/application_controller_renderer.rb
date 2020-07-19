10db219d5313b1d58a94065baa7269e907870e92
vagrant <morio4899@gmail.com>
2020-07-19 02:49:14 +0000

    user_edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '33e6e4f00891d17627c8a230d247c7251dab6a7dbee08f03f8d442be6a2d6fcf56294599ed42ffdf41e52fa26a1d08600021b112ba432b36bc023fbc79efdb32'
