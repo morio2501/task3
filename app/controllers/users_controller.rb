ac0a49289e81882305a8021b5db1ab919decca8e
vagrant <morio4899@gmail.com>
2020-07-21 01:34:34 +0000

    books_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
 		@books = @user.books
 		@book = Book.new
 	end
+
 	def index
 		@users = User.all
 		@book = Book.new
 	end
+	
 	def edit
 		@user = User.find(params[:id])
 	end
